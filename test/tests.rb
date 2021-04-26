d65bb78c93b553a7ee7ed3c5a514d27ec419127e
Vee Satayamas <5ssgdxltv@relay.firefox.com>
2021-04-26 18:38:34 +0700

    test

---
+++ test/tests.rb
@@ -0,0 +1,26 @@
+# coding: utf-8
+require "test/unit"
+require_relative "../lib/thai_romanize"
+
+class RomanizeTest < Test::Unit::TestCase
+  def test_simple    
+    assert_equal "ka", ThaiRomanize.romanize("กา")
+  end
+
+  def test_two_words
+    assert_equal "ka ka", ThaiRomanize.romanize("กากา")
+  end
+
+  def test_kan
+    assert_equal "kan", ThaiRomanize.romanize("การ")
+  end
+
+  def test_complex_word
+    assert ThaiRomanize.romanize("อานิสงส์").length > 0
+  end
+
+  def test_long
+    assert ThaiRomanize.romanize("อานิสงส์ของการได้ยินได้ฟังพุทธวจนก่อนตาย เป็นอย่างไร").length > 0
+  end
+
+end
